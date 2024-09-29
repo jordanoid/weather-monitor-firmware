c3998d20838501bf047d05ef331b1828b606e54a
Jordano Iqbal Darmawan <79011617+jordanoid@users.noreply.github.com>
2024-09-29 09:26:12 +0700

    i messed up the main.c lol

--- main/main.c
+++ main/main.c
@@ -8,6 +8,20 @@
 #include "freertos/semphr.h"
 #include "freertos/queue.h"
 
+float uvValue;
+static const char TAG[] = "weather monitor";
+
+void readUV(void *pvParameters){
+    char buffer[15];
+    ml8511_init(ADC_UNIT_1, ADC_CHANNEL_7);
+    for(;;){
+        get_uv_intensity(ADC_CHANNEL_7, &uvValue);
+        sprintf(buffer, "UV: %.6f", uvValue);
+        esp_log_buffer_char(TAG, buffer, sizeof(buffer)/sizeof(char));
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
 void app_main(void)
 {
     ESP_LOGI(TAG, "Test Main");
