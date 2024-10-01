9258406345cd2f4c35c34c0efac84ac2ed65a63b
Jordano Iqbal Darmawan <79011617+jordanoid@users.noreply.github.com>
2024-10-02 06:15:43 +0700

    bmp280 prototype on prog 2

--- components/bmp280/bmp280_i2c.c
+++ components/bmp280/bmp280_i2c.c
@@ -2,6 +2,7 @@
 #include "bmp280_i2c.h"
 
 i2c_master_bus_handle_t master_handle;
+i2c_master_dev_handle_t bmp280_handle;
 
 void i2c_master_init(uint8_t SCL_IO_PIN, uint8_t SDA_IO_PIN){
     i2c_master_bus_config_t i2c_master_config = {
@@ -16,9 +17,16 @@ void i2c_master_init(uint8_t SCL_IO_PIN, uint8_t SDA_IO_PIN){
 };
 
 void bmp280_i2c_init(){
+
     i2c_device_config_t bmp280_cfg = {
         .dev_addr_length = I2C_ADDR_BIT_LEN_7,
         .device_address = 0x76,
         .scl_speed_hz = 200000,
     };
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(master_handle, &bmp280_cfg, &bmp280_handle));\
+    /*
+    Needs to setup mode and bmp280 config, for continous reading normal mode is preferred
+
+    
+    */
 };
--- components/bmp280/include/bmp280_i2c.h
+++ components/bmp280/include/bmp280_i2c.h
@@ -3,6 +3,54 @@
 
 #include "driver/i2c_master.h"
 
+#define BMP280_DEFAULT_CHIP_ID    0X58
+#define BMP280_RESET_VALUE        0XB6
+
+#define SEA_LEVEL_PRESSURE        101325
+
+#define ID_ADDRESS                0XD0
+#define RESET_ADDRESS             0XE0
+#define STATUS_ADDRESS            0XF3
+#define CTRL_MEAS_ADDRESS         0XF4
+#define CONFIG_ADDRESS            0XF5
+#define PRESSURE_MSB              0xF7
+#define PRESSURE_LSB              0xF8
+#define PRESSURE_XLSB             0xF9
+#define TEMP_MSB                  0xFA
+#define TEMP_LSB                  0xFB
+#define TEMP_XLSB                 0xFC
+
+#define IM_UPDATE_BIT             0X00
+#define MEASURING_BIT             0X01
+#define MODE_BIT                  0X00
+#define OSRS_P_BIT                0X02
+#define OSRS_T_BIT                0X05
+#define SPI3W_EN_BIT              0X00
+#define FILTER_BIT                0X02
+#define T_SB_BIT                  0X05
+
+#define IM_UPDATE_LENGTH          0X01
+#define MEASURING_LENGTH          0X03
+#define MODE_LENGTH               0X02
+#define OSRS_P_LENGTH             0X03
+#define OSRS_T_LENGTH             0X03
+#define SPI3W_EN_LENGTH           0X01
+#define FILTER_LENGTH             0X03
+#define T_SB_LENGTH               0X03
+
+#define T1_ADDRESS                0X88
+#define T2_ADDRESS                0X8A
+#define T3_ADDRESS                0X8C
+#define P1_ADDRESS                0X8E
+#define P2_ADDRESS                0X90
+#define P3_ADDRESS                0X92
+#define P4_ADDRESS                0X94
+#define P5_ADDRESS                0X96
+#define P6_ADDRESS                0X98
+#define P7_ADDRESS                0X9A
+#define P8_ADDRESS                0X9C
+#define P9_ADDRESS                0X9E
+
 void i2c_master_init(uint8_t SCL_IO_PIN, uint8_t SDA_IO_PIN);
 
 void bmp280_i2c_init(void);
