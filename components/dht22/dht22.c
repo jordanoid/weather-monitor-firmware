9faadfd88c605fe1c57e9a0e84eec78beb0c3ec4
Jordano Iqbal Darmawan <79011617+jordanoid@users.noreply.github.com>
2024-10-17 07:08:45 +0700

    dht22 proto on prog

--- components/dht22/dht22.c
+++ components/dht22/dht22.c
@@ -16,6 +16,16 @@ void dht22_set_pin (gpio_num_t gpio_pin){
     dht22_pin = gpio_pin;
 }
 
+static bool check_response( uint8_t duration, bool state){
+    uint8_t loop = 0;
+    while(gpio_get_level(dht22_pin) != state){
+        if(loop > duration){return 0;}
+        loop++;
+        delay_us(1);
+    }
+    return 1;
+}
+
 void dht22_read(float *temp, float *hum){
 
     uint8_t raw_data[5];
@@ -23,12 +33,14 @@ void dht22_read(float *temp, float *hum){
     //starting
     gpio_set_direction(dht22_pin, GPIO_MODE_OUTPUT);
     gpio_set_level(dht22_pin, 0);
-    delay_us(2000);
+    delay_us(1500);
     gpio_set_level(dht22_pin, 1);
     delay_us(30);
 
     //check connection
+    gpio_set_direction(dht22_pin, GPIO_MODE_INPUT);
+    if(check_response(85, 1) == 1)
     
-
 }
+
 
